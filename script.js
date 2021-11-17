1b37f4571d1af6951241808543deb0b28892773c
Lauren Leslie <laurenlesliesilva@gmail.com>
2021-11-16 22:46:36 -0300

    Deixando o usuário escolher quantas casas demais

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 let min = document.getElementById("min");
 let max = document.getElementById("max");
 let negative = document.getElementById("negativeNumbers");
-let decimal = document.getElementById("decimalNumbers")
+let decimal = document.getElementById("decimalNumbers");
+let decimalPlaces = document.getElementById("decimalPlaces");
+
 
 function generate() {
-
     let minValue = parseInt(min.value);
     let maxValue = parseInt(max.value);
+    let decimalPlacesValue = parseInt(decimalPlaces.value);
     let randomNumber;
 
-    if (minValue < maxValue) {
-        randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+    if(!minValue || !maxValue)
+        alert("Please insert values")
+    if (minValue > maxValue) 
+        alert("Min value must be less than max value");
+    else if (decimal.checked === true) {
+        randomNumber = parseFloat(Math.random() * (maxValue - minValue) + minValue).toFixed(decimalPlacesValue);
     }
     else {
-        alert("Min value must be less than max value");
+        randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
     }
     document.getElementById("result").innerHTML = `Result: ${randomNumber}`;
 
